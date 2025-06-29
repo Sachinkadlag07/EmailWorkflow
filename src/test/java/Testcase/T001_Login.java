5cd9d9777347aa6256b0287482d6ddbbaa5e2419
Sachinkadlag07 <sachinkadlag07@gmail.com>
2025-06-29 22:53:12 +0530

    adding log4j xml and basetest code line

--- src/test/java/Testcase/T001_Login.java
+++ src/test/java/Testcase/T001_Login.java
@@ -9,9 +9,12 @@ public class T001_Login extends Basetest {
 
     @Test(groups ="sanity")
     public void logintoEmail() throws InterruptedException {
+
+        logger.info("**** Starting T001 ****");
         SignInPage sp=new SignInPage(driver);
-        //logger.info("Entering email id");
+        logger.info("Entering Gmail id in to text filed");
         sp.enterEmailId(p.getProperty("Emailid"));
+        logger.info(("clicking on Next CTA"));
         sp.clickNext();
         Thread.sleep(10000);
         sp.enterPassword("miyamo123");
